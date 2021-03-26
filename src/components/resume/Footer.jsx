fb419eed0caeca3c47f14fa26442d6ce9b43487b
Sumet Phongphila <notsumet1@gmail.com>
2021-03-27 02:22:53 +0700

    add animation in footer

--- src/components/resume/Footer.jsx
+++ src/components/resume/Footer.jsx
@@ -3,7 +3,7 @@ import { FaGithubAlt } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="w-full mt-20 py-10 bg-gray-800">
+    <footer className="w-full mt-20 py-10 bg-gray-800 animate__animated animate__fadeInUp">
       <a href="https://github.com/sumetph" target="_black">
         <div className="flex justify-center items-center">
           <FaGithubAlt size={30} />
