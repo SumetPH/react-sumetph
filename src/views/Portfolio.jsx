358d5e3a9f2dba38976b9286c948aea49231471f
sumetph <notsumet1@gmail.com>
2021-03-29 22:20:02 +0700

    fix last commit

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Box from "../components/portfolio/Box";
 
-import resume from "../assets/website/resume.jpg";
-// import project from "../assets/website/project.jpg";
+// import resume from "../assets/website/resume.jpg";
+import project from "../assets/website/project.jpg";
 import restaurant from "../assets/website/restaurant.jpg";
 import isr from "../assets/website/isr.jpg";
 import twitter from "../assets/website/twitter.jpg";
@@ -24,7 +24,7 @@ export default function portfolio() {
         Hover the image for information.
       </small>
       <div className="animate__animated animate__fadeInUp grid grid-cols-1 md:grid-cols-2 gap-4">
-        <Box
+        {/* <Box
           img={resume}
           title="SumetPH's Resume"
           frontEnd="React.js"
@@ -34,8 +34,8 @@ export default function portfolio() {
           host="Github"
           repo="https://github.com/SumetPH/react-resume"
           website="https://sumetph.github.io/"
-        />
-        {/* <Box
+        /> */}
+        <Box
           img={project}
           title="DDS Rmuti"
           frontEnd="Vue.js"
@@ -45,7 +45,7 @@ export default function portfolio() {
           host="000webhostapp"
           repo="https://github.com/SumetPH/laravel-graduation-project"
           website="https://rmuti-dds.000webhostapp.com/"
-        /> */}
+        />
         <Box
           img={isr}
           title="ISR Rmuti"
